3e0f3d16fb4e93db632f833f229f0523485a03a5
Algaron <79378904+Algaron@users.noreply.github.com>
2022-12-15 15:43:49 +0100

    Changed to singular form and added a list dances to songs

--- LineDanceWF/Data/Dance.cs
+++ LineDanceWF/Data/Dance.cs
@@ -2,11 +2,11 @@
 
 namespace LineDanceWF.Data
 {
-    internal class Dances
+    internal class Dance
     {
         public int DanceId { get; set; }
         public string? Name { get; set; } = string.Empty;
-        public Songs? SourceSong { get; set; }
-        public virtual ObservableCollectionListSource<Songs>? Alternatives { get; set; }
+        public Song? SourceSong { get; set; }
+        public virtual ObservableCollectionListSource<Song>? Alternatives { get; set; }
     }
 }
--- LineDanceWF/Data/Song.cs
+++ LineDanceWF/Data/Song.cs
@@ -1,11 +1,13 @@
-﻿namespace LineDanceWF.Data
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace LineDanceWF.Data
 {
-    internal class Songs
+    internal class Song
     {
         public int SongId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string FileHash { get; set; } = string.Empty;
         public string FilePath { get; set; } = string.Empty;
-
+        public virtual ObservableCollectionListSource<Dance>? Dances { get; set; }
     }
 }
