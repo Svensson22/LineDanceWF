c94d96459c39560a06768757913f2496b971bc48
Svensson22 <xeno.zohar@gmail.com>
2022-12-15 13:00:41 +0100

    Städa usings och gör Alternatives observable

--- LineDanceWF/Data/Dances.cs
+++ LineDanceWF/Data/Dances.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace LineDanceWF.Data
 {
@@ -12,6 +7,6 @@ namespace LineDanceWF.Data
         public int DanceId { get; set; }
         public string? Name { get; set; } = string.Empty;
         public Songs? SourceSong { get; set; }
-        public virtual List<Songs>? Alternatives { get; set; }
+        public virtual ObservableCollectionListSource<Songs>? Alternatives { get; set; }
     }
 }
