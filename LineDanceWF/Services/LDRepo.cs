5ea9a107593fa381c660eed519a9de4cb203ed0f
Svensson22 <xeno.zohar@gmail.com>
2022-12-15 13:50:53 +0100

    Repo-lager

---
+++ LineDanceWF/Services/LDRepo.cs
@@ -0,0 +1,18 @@
+﻿namespace LineDanceWF.Services
+{
+    internal class LDRepo
+    {
+        private LDContext _db;
+
+        public LDRepo(LDContext db)
+        {
+            _db = db;
+        }
+
+        public void DemoMetod()
+        {
+            // Prata med injectad dbcontext
+            var x = _db.Songs;
+        }
+    }
+}
