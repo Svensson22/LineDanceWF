262385a3c3fbf0026011892b66f906c71553d73d
Algaron <79378904+Algaron@users.noreply.github.com>
2022-12-15 18:45:27 +0100

    Added basic CUD repo functions for song and dance

--- LineDanceWF/Services/LDRepo.cs
+++ LineDanceWF/Services/LDRepo.cs
@@ -1,4 +1,6 @@
-﻿namespace LineDanceWF.Services
+﻿using LineDanceWF.Data;
+
+namespace LineDanceWF.Services
 {
     internal class LDRepo
     {
@@ -14,5 +16,38 @@
             // Prata med injectad dbcontext
             var x = _db.Songs;
         }
+
+        public void AddSong(Song song)
+        {
+            _db.Songs.Add(song);
+            _db.SaveChanges();
+        }
+        public void DeleteSong(Song song)
+        {
+            _db.Songs.Remove(song);
+            _db.SaveChanges();
+        }
+        public void EditSong(Song song)
+        {
+            _db.Songs.Update(song);
+            _db.SaveChanges();
+        }
+        public void AddDance(Dance dance)
+        {
+            _db.Dances.Add(dance);
+            _db.SaveChanges();
+        }
+        public void DeleteDance(Dance dance)
+        {
+            _db.Dances.Remove(dance);
+            _db.SaveChanges();
+        }
+        public void EditDance(Dance dance)
+        {
+            _db.Dances.Update(dance);
+            _db.SaveChanges();
+        }
+        
+
     }
 }
