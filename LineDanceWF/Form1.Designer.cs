5d05280447892485934881b0fbfff2d79bc3ca63
Algaron <79378904+Algaron@users.noreply.github.com>
2022-12-18 14:11:30 +0100

    Start for GUI to show current problem

--- LineDanceWF/Form1.Designer.cs
+++ LineDanceWF/Form1.Designer.cs
@@ -31,7 +31,7 @@ namespace LineDanceWF
             //Delete later just testing to see were it stops
             //pick a folder were you have music to check if it adds
             //Atm issues with conection to database
-            lDRepo.AddSongsFromFolder();
+           // lDRepo.AddSongsFromFolder();
             base.OnLoad(e);
         }
         #region Windows Form Designer generated code
@@ -42,12 +42,75 @@ namespace LineDanceWF
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.DanceSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(2168, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // DanceSearch
+            // 
+            this.DanceSearch.Location = new System.Drawing.Point(35, 316);
+            this.DanceSearch.Name = "DanceSearch";
+            this.DanceSearch.Size = new System.Drawing.Size(227, 27);
+            this.DanceSearch.TabIndex = 1;
+            this.DanceSearch.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 281);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Dances";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(37, 365);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(283, 484);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(2168, 1272);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DanceSearch);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        
+        private PictureBox pictureBox1;
+        private TextBox DanceSearch;
+        private Label label1;
+        private ListBox listBox1;
+        
     }
 }
--- LineDanceWF/Form1.cs
+++ LineDanceWF/Form1.cs
@@ -1,3 +1,7 @@
+using LineDanceWF.Data;
+using LineDanceWF.Services;
+using System.Security.Cryptography.X509Certificates;
+
 namespace LineDanceWF
 {
     public partial class Form1 : Form
@@ -5,6 +9,22 @@ namespace LineDanceWF
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LDContext dbtest = new LDContext();
+            //db.Database.EnsureDeleted();
+            dbtest.Database.EnsureCreated();
+            LDRepo lDRepo = new LDRepo(dbtest);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            List<Song> songs = new List<Song>();
+            string searchText = DanceSearch.Text;
+           
         }
     }
 }
